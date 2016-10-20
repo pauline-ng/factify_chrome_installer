0d8b10c9396579c4bee1a1c95b408210c70b351b
Sun Sagong <suns1@gis.a-star.edu.sg>
2016-10-20 13:38:58 +0800

    Add Mac/Linux installer

--- install_scripts_mac_linux/setup.sh
+++ install_scripts_mac_linux/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 set -e
-DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # Step 1: Check Java version 1.8+
  
@@ -32,13 +31,10 @@ fi
 
 # Step 2: Ask user to installation.
 
-echo "It will require 50MB of the disk space."
-echo "Do you want to proceed?(yes/no):"
+echo "It will require 48MB of the disk space."
+echo "Press any key to proceed?(yes/no):"
 read ans
 
-if ["$ans" != "yes"]; then
-  exit 0
-fi
 
 # Step 3: Create factpub folder under $HOME
 
@@ -53,42 +49,44 @@ mkdir $HOST_DIR
 # Step 4: Download necessary files from factpub.org to the working folder
 
 curl -o $HOST_DIR/org.factpub.factify.json http://factpub.org/public/factify_ChromeExtension/install_scripts_mac_linux/org.factpub.factify.json
-curl -o $HOST_DIR/factify_launcher.sh http://factpub.org/public/factify_ChromeExtension/install_scripts_mac_linux/factify_launcher.sh
-curl -o $HOST_DIR/uninstall.sh http://factpub.org/public/factify_ChromeExtension/install_scripts_mac_linux/uninstall.sh
+curl -o $HOST_DIR/factify_launcher.sh http://factpub.org/public/factify_ChromeExtension/install_scripts_mac_linux/factify_launcher
+curl -o $HOST_DIR/uninstall.sh http://factpub.org/public/factify_ChromeExtension/install_scripts_mac_linux/uninstall
 curl -o $HOST_DIR/factify.jar http://factpub.org/public/factify_ChromeExtension/_factify.jar
 
+chmod 755 $HOST_DIR/factify_launcher.sh
+chmod 755 $HOST_DIR/uninstall.sh
 
 # Step 5: Register the host program.
 
-# if [ "$(uname -s)" = "Darwin" ]; then
-#   if [ "$(whoami)" = "root" ]; then
-#     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
-#   else
-#     TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
-#   fi
-# else
-#   if [ "$(whoami)" = "root" ]; then
-#     TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
-#   else
-#     TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
-#   fi
-# fi
-# HOST_NAME=org.factpub.factify
-# # Create directory to store native messaging host.
-# mkdir -p "$TARGET_DIR"
-# # Copy native messaging host manifest.
-# cp "$DIR/$HOST_NAME.json" "$TARGET_DIR"
+if [ "$(uname -s)" = "Darwin" ]; then
+  if [ "$(whoami)" = "root" ]; then
+    TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
+  else
+    TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+  fi
+else
+  if [ "$(whoami)" = "root" ]; then
+    TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
+  else
+    TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
+  fi
+fi
+HOST_NAME=org.factpub.factify
+# Create directory to store native messaging host.
+mkdir -p "$TARGET_DIR"
+# Copy native messaging host manifest.
+cp "$HOST_DIR/$HOST_NAME.json" "$TARGET_DIR"
 
-# # Update host path in the manifest.
-# HOST_PATH=
-# ESCAPED_HOST_PATH=${HOST_PATH////\\/}
+# Update host path in the manifest.
+HOST_PATH="$HOST_DIR/factify_launcher.sh"
+ESCAPED_HOST_PATH=${HOST_PATH////\\/}
 
-# # Substitute the HOST_PATH with actual path of the host program
-# sed -i -e "s/HOST_PATH/$ESCAPED_HOST_PATH/" "$TARGET_DIR/$HOST_NAME.json"
+# Substitute the HOST_PATH with actual path of the host program
+sed -i -e "s/HOST_PATH/$ESCAPED_HOST_PATH/" "$TARGET_DIR/$HOST_NAME.json"
 
-# # Set permissions for the manifest so that all users can read it.
-# chmod o+r "$TARGET_DIR/$HOST_NAME.json"
-# echo "Native messaging host $HOST_NAME has been installed."
+# Set permissions for the manifest so that all users can read it.
+chmod o+r "$TARGET_DIR/$HOST_NAME.json"
+echo "Native messaging host $HOST_NAME has been installed."
 
 
 # Step 6: Wait till user input and close.
